47ecbe1c093b4c5d1a2dd9de9318e4da3c160aa4
DESKTOP-C5CSPL2\dev030 <jcw9828@gmail.com>
2022-01-17 18:55:59 +0900

    메인페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,56 +1,69 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
+import Data from './data';
 
 function App() {
+
+  let [shoes, setShoes] = useState(Data);
+
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
       <div className="background" style={{ width: '100%', height: '400px' }}>
         <h1>20% Season Off</h1>
       </div>
 
+    {/* 컴포넌트 map 반복문 활용 */}
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
+          {
+            shoes.map((e, i) => {
+              return (<Card shoes={shoes[i]} i={i} />
+              )
+            })
+          }
         </div>
       </div>
-
     </div>
   );
+}
+
+// 컴포넌트 만들기
+function Card(props) {
+  return (
+    <div>
+      <div className="col-md-4">
+        <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="100%" />
+        <h4>{props.shoes.title}</h4>
+        <p>{props.shoes.content} & {props.shoes.price}</p>
+      </div>
+    </div>
+
+  )
 }
 
 export default App;
